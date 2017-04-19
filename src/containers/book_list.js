6eeee38fbde0cfa0695f20296fa12e18d7946023
evan <stewarea@gmail.com>
2017-04-18 18:24:40 -0700

    build initial compnent and continer

---
+++ src/containers/book_list.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+//connect react redux is the glue between React and Redux
+
+class BookList extends Component {
+  renderList() {
+    return this.props.books.map((book) => {
+      return (
+        <li key={book.title} className="list-group-item">{book.title}</li>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <ul className="list-group col-sm-4">
+        {this.renderList()}
+      </ul>
+    )
+  }
+}
+
+function mapStateToProps(state) {
+  //whateveris returned will show up as props inside of BookList
+  return{
+    books: state.books
+  };
+}
+
+export default connect(mapStateToProps)(BookList);
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    {title: 'book1' },
+    {title: 'book2' },
+    {title: 'book3' },
+    {title: 'book4' },
+  ]
+}
